443cfb4e0a78e8bc92618ced3a775c15d6067d68
Daniel Szemerey <szemy2@gmail.com>
2022-01-28 20:25:30 +0100

    Create replace_functions.py

---
+++ mycodemod/replace_functions.py
@@ -0,0 +1,52 @@
+import argparse
+from ast import Expression, literal_eval
+from typing import Union
+
+import libcst as cst
+from libcst.codemod import CodemodContext, VisitorBasedCodemodCommand
+from libcst.codemod.visitors import AddImportsVisitor
+
+
+class ReplaceFunctionCommand(VisitorBasedCodemodCommand):
+
+    # Add a description so that future codemodders can see what this does.
+    DESCRIPTION: str = "Replaces the body of a function with pass."
+
+    # @staticmethod
+    # def add_args(arg_parser: argparse.ArgumentParser) -> None:
+    #     # Add command-line args that a user can specify for running this
+    #     # codemod.
+    #     arg_parser.add_argument(
+    #         "--string",
+    #         dest="string",
+    #         metavar="STRING",
+    #         help="String contents that we should look for.",
+    #         type=str,
+    #         required=True,
+    #     )
+    #     arg_parser.add_argument(
+    #         "--constant",
+    #         dest="constant",
+    #         metavar="CONSTANT",
+    #         help="Constant identifier we should replace strings with.",
+    #         type=str,
+    #         required=True,
+    #     )
+
+    def __init__(self, context: CodemodContext) -> None:
+        # Initialize the base class with context, and save our args. Remember, the
+        # "dest" for each argument we added above must match a parameter name in
+        # this init.
+        print("this happens")
+        super().__init__(context)
+        # self.string = string
+        # self.constant = constant
+
+    def leave_FunctionDef(self, original_node: cst.FunctionDef, updated_node: cst.FunctionDef) -> cst.FunctionDef:
+        replace_function = cst.FunctionDef(
+            name=updated_node.name, 
+            params=cst.Parameters(), 
+            body= cst.SimpleStatementSuite((cst.Pass(),)), 
+            returns=None)
+        
+        return replace_function
